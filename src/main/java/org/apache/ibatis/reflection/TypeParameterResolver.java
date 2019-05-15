fc691e036692289661a0b859a364881dbbe70848
cheney <478241123@qq.com>
2019-05-15 17:35:09 +0800

    [update]

--- src/main/java/org/apache/ibatis/reflection/TypeParameterResolver.java
+++ src/main/java/org/apache/ibatis/reflection/TypeParameterResolver.java
@@ -126,6 +126,21 @@ public class TypeParameterResolver {
     return result;
   }
 
+  /**
+   * 获取类上所有泛型擦除后的类型的方式(这里的擦除是指继承时将泛型参数具象化,
+   * 例:TypeTestDto3 extend TypeTestDto2<Integer>,TypeTestDto2<R> extend TypeTestDto<String,R>此时泛型参数具象化后为<String,Integer>
+   * <p>
+   * 首次进入方法: srcType为TypeTestDto3_class,调用scanSuperTypes进行递归
+   * 从第一个父类开始向父级递归：
+   * 递归方法：第一次递归 -> TypeTestDto2_parameterizedType.getRawType() ->TypeTestDto2_class.getTypeParameters() 获取所有原始泛型 -> R
+   * TypeTestDto2_parameterizedType.getActualTypeArguments() 获取所有泛型参数 -> Integer
+   * <p>
+   * TypeTestDto2_class.getGenericSuperclass() -> TypeTestDto_parameterizedType.getActualTypeArguments() -> <String,R>
+   * ->判断R == TypeTestDto2_class原始泛型的R -> 得出R具象化后为Integer类型 -> 得出<String,Integer>
+   * <p>
+   * 若TypeTestDto还有superClass则继续递归。
+   * 此时最终返回值为ParameterizedTypeImpl{rawType:TypeTestDto3.class,actualTypeArguments:[String.class,Integer.class]}
+   */
   private static Type resolveTypeVar(TypeVariable<?> typeVar, Type srcType, Class<?> declaringClass) {
     Type result;
     Class<?> clazz;
@@ -167,37 +182,58 @@ public class TypeParameterResolver {
 
   private static Type scanSuperTypes(TypeVariable<?> typeVar, Type srcType, Class<?> declaringClass, Class<?> clazz, Type superclass) {
     if (superclass instanceof ParameterizedType) {
+      //若superclass是ParameterizedType类型，则直尝试查找actualTypeArguments
       ParameterizedType parentAsType = (ParameterizedType) superclass;
       Class<?> parentAsClass = (Class<?>) parentAsType.getRawType();
+      //class.getTypeParameters():获取原始泛型
       TypeVariable<?>[] parentTypeVars = parentAsClass.getTypeParameters();
       if (srcType instanceof ParameterizedType) {
+        //目标类也是ParameterizedType，则尝试将所有真实泛型提取出来
         parentAsType = translateParentTypeVars((ParameterizedType) srcType, clazz, parentAsType);
       }
+      //
       if (declaringClass == parentAsClass) {
         for (int i = 0; i < parentTypeVars.length; i++) {
           if (typeVar == parentTypeVars[i]) {
+            //递归出口，执行条件：目标类的父类已经是declaringClass，则尝试匹配typeVar(所求的type)，成功则返回type的对应的实际类型
             return parentAsType.getActualTypeArguments()[i];
           }
         }
       }
       if (declaringClass.isAssignableFrom(parentAsClass)) {
+        //递归：parentAsClass是declaringClass的子类则递归继续擦除泛型
         return resolveTypeVar(typeVar, parentAsType, declaringClass);
       }
     } else if (superclass instanceof Class && declaringClass.isAssignableFrom((Class<?>) superclass)) {
+      //若superclass是class类型，并且是declaringClass（所求的type所在的类）的子类
+      //则直接向上递归无需查找actualTypeArguments
       return resolveTypeVar(typeVar, superclass, declaringClass);
     }
     return null;
   }
 
+  /**
+   * 执行此步骤：
+   * 1、TypeTestDto_parameterizedType（parentType）.getActualTypeArguments() -> <String,R>
+   *
+   * 2、TypeTestDto2_parameterizedType（srcType）.getActualTypeArguments() -> <Integer>
+   * 3、TypeTestDto2_class(srcClass).getTypeParameters() -> <R>
+   *
+   * 4、判断1中R == 3中TypeTestDto2_class原始泛型的R -> 得出R具象化后为Integer类型 -> 得出<String,Integer>
+   */
   private static ParameterizedType translateParentTypeVars(ParameterizedType srcType, Class<?> srcClass, ParameterizedType parentType) {
+    //1.目标类父类的实际泛型参数
     Type[] parentTypeArgs = parentType.getActualTypeArguments();
+    //2.目标类的的实际泛型参数
     Type[] srcTypeArgs = srcType.getActualTypeArguments();
+    //3.目标类原始泛型参数
     TypeVariable<?>[] srcTypeVars = srcClass.getTypeParameters();
     Type[] newParentArgs = new Type[parentTypeArgs.length];
     boolean noChange = true;
     for (int i = 0; i < parentTypeArgs.length; i++) {
       if (parentTypeArgs[i] instanceof TypeVariable) {
         for (int j = 0; j < srcTypeVars.length; j++) {
+          //4 判断是否有相同的TypeVariable,有则将实际类型存放到新的newParentArgs替换原始类型，并最后以newParentArgs为actualTypeArguments返回
           if (srcTypeVars[j] == parentTypeArgs[i]) {
             noChange = false;
             newParentArgs[i] = srcTypeArgs[j];
